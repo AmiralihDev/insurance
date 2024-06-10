9b159514265d366230f409e66cbc36704d597ba7
AmiralihDev <ophmmh.uib@gmail.com>
2024-06-10 16:25:26 +0330

    fix : update get year modules

--- src/js/yearCreateor.js
+++ src/js/yearCreateor.js
@@ -1,15 +1,14 @@
 import { showData } from "./showData"
 
 function yearCreateor(yearList){
-    let date = new Date
-    //get en year
-    let enYear = date.getFullYear()
-    //change to fa year
-    let FaYear = enYear - 621
+    let date = new Date().toLocaleDateString('fa-IR-u-nu-latn')
+    // get year from date
+    let FaYear = date.slice(0,4) 
+
     //Make year
     for (let index = 0; index < 21; index++) {
         //make 20years later
-        let year = FaYear - index
+        let year = parseInt(FaYear) - index
         let s = new showData
         //show year
         s.showYear(year)
